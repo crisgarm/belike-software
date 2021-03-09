a2024e8b467b116550841f85a53d71893e12a679
crisgarm <crisgarm9@gmail.com>
2021-03-09 17:36:32 +0100

    Check password

--- js/main.js
+++ js/main.js
@@ -5,27 +5,36 @@ const username = document.querySelector(".js-username");
 const password = document.querySelector(".js-password");
 const confirmPassword = document.querySelector(".js-confirmPassword");
 
-//Functio to show input error
 function showError(input, message) {
   const small = input.nextElementSibling;
   small.classList.add("error");
   small.innerHTML = message;
 }
 
-//Function to show success outline
 function showSuccess(input) {
   const small = input.nextElementSibling;
   small.classList.remove("error");
   small.innerHTML = "";
 }
 
-//Function to email validation
 function checkUsername(input) {
   const usernameValidation = /^[A-Za-z0-9\s]+$/g;
   if (usernameValidation.test(input.value.trim())) {
     showSuccess(input);
   } else {
     showError(input, "El nombre del usuario no es válido");
+  }
+}
+
+function checkPassword(input) {
+  const passwordValidation = /^(?=(?:.*[A-Z]){1})(?=(?:.*[#]))\S{7,}$/;
+  if (passwordValidation.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(
+      input,
+      "La contraseña debe tener mínimo 7 caracteres, una mayúscula y una #"
+    );
   }
 }
 
